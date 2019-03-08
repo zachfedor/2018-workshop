82eaef6ab2bc672660c0809802c2d59ed1cf3c31
zachfedor <zachfedor@gmail.com>
2019-03-08 12:09:07 -0500

    Add pytest setup, fixtures, and tests
    - Install pytest and coverage
    - Add conftest.py and setup.cfg files to configure pytest and coverage
    - Create mock data in temp database for tests
    - Add tests for all auth and blog views, plus tests for db and app
    factory functions which amounts to ~100% coverage
    - Small fix to error messages without periods

--- flaskr/flaskr/blog.py
+++ flaskr/flaskr/blog.py
@@ -74,7 +74,7 @@ def update(id):
         error = None
 
         if not title:
-            error = 'Title is required'
+            error = 'Title is required.'
         
         if error is not None:
             flash(error)
